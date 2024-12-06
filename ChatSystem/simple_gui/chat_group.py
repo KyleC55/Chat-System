1fea05ed44cd9993896369a6c71458a4def774ea
Kyle Chen <145894617+KyleC55@users.noreply.github.com>
2024-12-06 19:41:40 +0800

    group update

--- ChatSystem/simple_gui/chat_group.py
+++ ChatSystem/simple_gui/chat_group.py
@@ -1,7 +1,7 @@
-S_ALONE = 0
-S_TALKING = 1
 
-# ==============================================================================
+S_TALKING = 1
+S_ALONE = 0
+#==============================================================================
 # Group class:
 # member fields:
 #   - An array of items, each a Member class
@@ -13,10 +13,7 @@ S_TALKING = 1
 #    - list_all: who is in the system, and the chat groups
 #    - connect: connect to a peer in a chat group, and become part of the group
 #    - disconnect: leave the chat group but stay in the system
-# ==============================================================================
-
-S_ALONE = 0
-S_TALKING = 1
+#==============================================================================
 
 class Group:
 
@@ -30,68 +27,55 @@ class Group:
         return
 
     def is_member(self, name):
-
-        # IMPLEMENTATION
-        # ---- start your code ---- #
         return name in self.members.keys()
-        # ---- end of your code --- #
 
-    # implement
     def leave(self, name):
-        """
-        leave the system, and the group
-        """
-        # IMPLEMENTATION
-        # ---- start your code ---- #
         self.disconnect(name)
         del self.members[name]
-        # ---- end of your code --- #
         return
 
     def find_group(self, name):
-        """
-        Auxiliary function internal to the class; return two
-        variables: whether "name" is in a group, and if true
-        the key to its group
-        """
-
         found = False
         group_key = 0
-        # IMPLEMENTATION
-        # ---- start your code ---- #
-        for i in self.chat_grps.keys():
-            if name in self.chat_grps[i]:
+        for k in self.chat_grps.keys():
+            if name in self.chat_grps[k]:
                 found = True
-                group_key = i 
+                group_key = k
                 break
-        # ---- end of your code --- #
         return found, group_key
 
     def connect(self, me, peer):
-        """
-        me is alone, connecting peer.
-        if peer is in a group, join it
-        otherwise, create a new group with you and your peer
-        """
+        peer_in_group = False
+        #if peer is in a group, join it
         peer_in_group, group_key = self.find_group(peer)
-
-        # IMPLEMENTATION
-        # ---- start your code ---- #
-        pass
-
-        # ---- end of your code --- #
+        if peer_in_group == True:
+            print(peer, "is talking already, connect!")
+            self.chat_grps[group_key].append(me)
+            self.members[me] = S_TALKING
+        else:
+            # otherwise, create a new group
+            print(peer, "is idle as well")
+            self.grp_ever += 1
+            group_key = self.grp_ever
+            self.chat_grps[group_key] = []
+            self.chat_grps[group_key].append(me)
+            self.chat_grps[group_key].append(peer)
+            self.members[me] = S_TALKING
+            self.members[peer] = S_TALKING
+        print(self.list_me(me))
         return
 
-    # implement
     def disconnect(self, me):
-        """
-        find myself in the group, quit, but stay in the system
-        """
-        # IMPLEMENTATION
-        # ---- start your code ---- #
-        pass
-
-        # ---- end of your code --- #
+        # find myself in the group, quit
+        in_group, group_key = self.find_group(me)
+        if in_group == True:
+            self.chat_grps[group_key].remove(me)
+            self.members[me] = S_ALONE
+            # peer may be the only one left as well...
+            if len(self.chat_grps[group_key]) == 1:
+                peer = self.chat_grps[group_key].pop()
+                self.members[peer] = S_ALONE
+                del self.chat_grps[group_key]
         return
 
     def list_all(self):
@@ -102,33 +86,32 @@ class Group:
         full_list += str(self.chat_grps) + "\n"
         return full_list
 
-    # implement
+    def list_all2(self, me):
+        print("Users: ------------")
+        print(self.members)
+        print("Groups: -----------")
+        print(self.chat_grps, "\n")
+        member_list = str(self.members)
+        grp_list = str(self.chat_grps)
+        return member_list, grp_list
+
     def list_me(self, me):
-        """
-        return a list, "me" followed by other peers in my group
-        """
-        my_list = []
-        # IMPLEMENTATION
-        # ---- start your code ---- #
-        pass
-
-        # ---- end of your code --- #
+        # return a list, "me" followed by other peers in my group
+        if me in self.members.keys():
+            my_list = []
+            my_list.append(me)
+            in_group, group_key = self.find_group(me)
+            if in_group == True:
+                for member in self.chat_grps[group_key]:
+                    if member != me:
+                        my_list.append(member)
         return my_list
-
 
 if __name__ == "__main__":
     g = Group()
     g.join('a')
     g.join('b')
-    g.join('c')
-    g.join('d')
     print(g.list_all())
-
+    g.list_all2('a')
     g.connect('a', 'b')
-    print(g.list_all())
-    g.connect('c', 'a')
-    print(g.list_all())
-    g.leave('c')
-    print(g.list_all())
-    g.disconnect('b')
     print(g.list_all())
